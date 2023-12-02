681dc6da005dda7700d593347bcf9eda5481c734
trnKhanh <tnk29082003@gmail.com>
2023-12-02 18:05:03 +0700

    Only admin can create courses

--- server/controllers/Courses.controller.js
+++ server/controllers/Courses.controller.js
@@ -1,4 +1,5 @@
 const Course = require("../models/Courses.model");
+const Admin = require("../models/Admins.model");
 
 exports.create = (req, res) => {
   if (!req.body) {
@@ -7,19 +8,36 @@ exports.create = (req, res) => {
     });
     return;
   }
+  let userId = req.user.id;
+  let course = new Course({ ...req.body, admin_id: req.user.id });
 
-  let course = new Course({...req.body, admin_id: req.user.id});
-  Course.create(course, (err, course) => {
+  Admin.findOne({ id: userId }, (err, admin) => {
     if (err) {
       res.status(500).json({
-        message: err.message || "Errors occur when creating new course"
+        message: err.message || "Errors occur when finding admin"
       });
       return;
     }
 
-    res.json({
-      message: "New course is created successfully"
-    });
+    if (!admin) {
+      res.status(401).json({
+        message: "No permission to create new course"
+      });
+      return;
+    }
+
+    Course.create(course, (err, course) => {
+      if (err) {
+        res.status(500).json({
+          message: err.message || "Errors occur when creating new course"
+        });
+        return;
+      }
+
+      res.json({
+        message: "New course is created successfully"
+      });
+    })
   })
 }
 
@@ -30,12 +48,12 @@ exports.update = (req, res) => {
     });
     return;
   }
-  
+
   let courseId = req.body.id;
   let userId = req.user.id;
   let fields = req.body.fields;
 
-  Course.findOne({id: courseId}, (err, course) => {
+  Course.findOne({ id: courseId }, (err, course) => {
     if (err) {
       res.status(500).json({
         message: err.message || "Errors occur when updating course"
@@ -50,7 +68,7 @@ exports.update = (req, res) => {
       return;
 
     }
-    
+
     if (course.admin_id != userId) {
       res.status(401).json({
         message: "No permission to update course"
@@ -65,7 +83,7 @@ exports.update = (req, res) => {
         })
         return;
       }
-      
+
       res.json({
         message: "Course has been updated"
       });
---
+++ server/models/Admins.model.js
@@ -0,0 +1,26 @@
+const sql = require("./db");
+
+const Admin = function(admin) {
+  this.id = id;
+}
+
+Admin.findOne = function(filter, callback) {
+  sql.query("SELECT * FROM admins WHERE ?", filter, (err, res) => {
+    if (err) {
+      console.log(err);
+      callback(err, null);
+      return;
+    }
+
+    if (res.length) {
+      console.log("Find admin", {filter: filter, admins: res[0]});
+      callback(null, res[0]);
+      return;
+    }
+
+    console.log("Find no admin", {filter: filter});
+    callback(null, null);
+  })
+}
+
+module.exports = Admin;
