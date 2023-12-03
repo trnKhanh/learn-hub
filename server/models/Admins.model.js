84466a464c689c39828f4eb327d02628aa045f5f
trnKhanh <tnk29082003@gmail.com>
2023-12-03 16:19:48 +0700

    Update Admin

--- server/models/Admins.model.js
+++ server/models/Admins.model.js
@@ -1,28 +1,33 @@
 const sql = require("./db");
-
+const { formatFilters } = require("../utils/query.utils");
 // Constructor
 const Admin = function (admin) {
   this.id = admin.id;
 };
 
 // Find 1 admin (mainly use to check if user is admin)
-Admin.findOne = function (filter, callback) {
-  sql.query("SELECT * FROM admins WHERE ?", filter, (err, res) => {
-    if (err) {
-      console.log(err);
-      callback(err, null);
-      return;
-    }
+Admin.findOne = function (filters, callback) {
+  const { filterKeys, filterValues } = formatFilters(filters);
+  sql.query(
+    `SELECT * FROM admins WHERE ${filterKeys}`,
+    filterValues,
+    (err, res) => {
+      if (err) {
+        console.log(err);
+        callback(err, null);
+        return;
+      }
 
-    if (res.length) {
-      console.log("Find admin", { filter: filter, admins: res[0] });
-      callback(null, res[0]);
-      return;
-    }
+      if (res.length) {
+        console.log("Find admin: ", { filters: filters, results: res[0] });
+        callback(null, res[0]);
+        return;
+      }
 
-    console.log("Find no admin", { filter: filter });
-    callback(null, null);
-  });
+      console.log("Find no admin: ", { filters: filters });
+      callback(null, null);
+    },
+  );
 };
 
 module.exports = Admin;
