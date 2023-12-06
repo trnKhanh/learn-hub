abe6d6e1b7d1f8c739dccea8575d4ac3ad4d257f
trnKhanh <tnk29082003@gmail.com>
2023-12-06 17:54:27 +0700

    Add supporters

---
+++ server/models/Supporters.model.js
@@ -0,0 +1,150 @@
+const sql = require("../database/db");
+const { formatFilters } = require("../utils/query.utils");
+// Constructor
+class Supporter {
+  constructor(supporter) {
+    this.id = supporter.id;
+    this.role = supporter.role;
+  }
+
+  static queryFields = `id, role`;
+
+  // Create new Supporter
+  static create = async (newSupporter) => {
+    const con = await sql.getConnection();
+    try {
+      await con.beginTransaction();
+
+      const [res, _] = await con.query(
+        `INSERT INTO supporters SET ?`,
+        newSupporter,
+      );
+
+      await con.commit();
+      sql.releaseConnection(con);
+
+      console.log("Created supporters: ", {
+        newSupporter: newSupporter,
+        results: res,
+      });
+      return newSupporter;
+    } catch (err) {
+      await con.rollback();
+      sql.releaseConnection(con);
+
+      throw err;
+    }
+  };
+
+  // Find one supporter by filter
+  static findOne = async (filters) => {
+    const { filterKeys, filterValues } = formatFilters(filters);
+    const [rows, fields] = await sql.query(
+      `SELECT ${Supporter.queryFields} 
+       FROM supporters NATURAL JOIN users 
+       WHERE ${filterKeys}`,
+      filterValues,
+    );
+    if (rows.length) {
+      console.log("Found supporter: ", { filters: filters, results: rows[0] });
+      return rows[0];
+    } else {
+      console.log("Found no supporter: ", { filters: filters });
+      return null;
+    }
+  };
+
+  // Find all supporters by filter
+  static findAll = async (filters) => {
+    const { filterKeys, filterValues } = formatFilters(filters);
+    const [rows, fields] = await sql.query(
+      `SELECT ${Supporter.queryFields} 
+       FROM supporters NATURAL JOIN users  
+       WHERE ${filterKeys}`,
+      filterValues,
+    );
+    console.log("Found supporters: ", { filters: filters, results: rows });
+    return rows;
+  };
+
+  static getAll = async () => {
+    const [rows, fields] = await sql.query(
+      `SELECT ${Supporter.queryFields} 
+       FROM supporters NATURAL JOIN users`,
+    );
+    console.log("Get all supporters: ", { results: rows });
+    return rows;
+  };
+
+  // Update supporter by filters
+  static updateById = async (id, columns) => {
+    const con = await sql.getConnection();
+
+    try {
+      const [res, _] = await con.query(
+        `UPDATE supporters SET ?
+        WHERE id=?`,
+        [columns, id],
+      );
+      const [rows, fields] = await con.query(
+        `SELECT ${Supporter.queryFields} 
+         FROM supporters 
+         WHERE id=?`,
+        [id],
+      );
+
+      console.log("Updated supporters by Id", {
+        id: id,
+        columns: columns,
+        results: res,
+      });
+
+      con.commit();
+      sql.releaseConnection(con);
+      if (res.affectedRows == 0) return null;
+      else return rows;
+    } catch (err) {
+      await con.rollback();
+      sql.releaseConnection(con);
+
+      throw err;
+    }
+  };
+
+  // Delete supporter by filters
+  static deleteById = async (id) => {
+    const con = await sql.getConnection();
+
+    try {
+      const [rows, fields] = await con.query(
+        `SELECT ${Supporter.queryFields} 
+         FROM supporters  
+         WHERE id=?`,
+        [id],
+      );
+
+      const [res, _] = await con.query(
+        `DELETE FROM supporters 
+        WHERE id=?`,
+        [id],
+      );
+
+      console.log("Deleted supporters by id", {
+        id: id,
+        results: res,
+      });
+
+      con.commit();
+      sql.releaseConnection(con);
+
+      if (res.affectedRows == 0) return null;
+      else return rows;
+    } catch (err) {
+      await con.rollback();
+      sql.releaseConnection(con);
+
+      throw err;
+    }
+  };
+}
+module.exports = Supporter;
