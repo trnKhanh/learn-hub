baf12dcde9d9fae7465e4ddf53813d8ca9665db3
trnKhanh <tnk29082003@gmail.com>
2024-01-22 10:48:56 +0700

    Fix: cannot load learning lessons

--- client/learn-hub/app/(course)/courses/[courseId]/learn/lessons/[lessonId]/page.tsx
+++ client/learn-hub/app/(course)/courses/[courseId]/learn/lessons/[lessonId]/page.tsx
@@ -19,61 +19,65 @@ import { CourseNavbar } from "../../_component/course-navbar";
 import { CourseSidebar } from "../../_component/course-sidebar";
 
 const LessonIdPage = ({
-    params
+  params,
 }: {
   params: { courseId: string; lessonId: string };
 }) => {
-    const { lessons } = useContext(LearnLessonContext);
-    const { course, isPurchased } = useContext(CourseContext);
+  const { lessons } = useContext(LearnLessonContext);
+  const { course, isPurchased } = useContext(CourseContext);
 
-    const [lesson, setLesson] = useState<LearnLesson>();
-    const [nextLesson, setNextLesson] = useState<LearnLesson>();
+  const [lesson, setLesson] = useState<LearnLesson>();
+  const [nextLesson, setNextLesson] = useState<LearnLesson>();
 
-    useEffect(() => {
-      if (lessons && course) {
-          setLesson(lessons.find((lesson) => lesson.id === params.lessonId));
-          setNextLesson(lessons.find((lesson) => lesson.id === (parseInt(params.lessonId) + 1).toString()));
-      }
-    }, [lessons, course]);
-
-    const [ documents, setDocuments ] = useState<CourseDocument[]>([]);
-
-    useEffect(() => {
-        getAllDocuments(params.courseId, params.lessonId).then((res) => {
-            if (res) {
-                if (res.status == 200) {
-                    setDocuments(res.data.documents);
-                }
-            }
-        });
-    }, []);
-
-    if (!lesson || !course) {
-        return <div>Loading...</div>;
+  useEffect(() => {
+    if (lessons && course) {
+      setLesson(lessons.find((lesson) => lesson.id == params.lessonId));
+      setNextLesson(
+        lessons.find(
+          (lesson) => lesson.id == (parseInt(params.lessonId) + 1).toString(),
+        ),
+      );
     }
+  }, [lessons, course]);
 
-    const isLocked = !lesson.isFree && !isPurchased;
-    const completeOnEnd = !!isPurchased && !lesson.finished_at;
+  const [documents, setDocuments] = useState<CourseDocument[]>([]);
 
-    return (
-        <div className="h-full">
-          <div className="md:pl-80 pt-[80px] h-full">
-            <div>
-              {lesson.finished_at && (
-                <Banner
-                    variant="success"
-                    label="You already completed this lesson."
-                />
-              )}
-              {isLocked && (
-                <Banner
-                    variant="warning"
-                    label="You need to purchase this course to watch this lesson."
-                />
-              )}
-            <div className="flex flex-col max-w-4xl mx-auto pb-20">
-              <div className="p-4">
-                  {/*<VideoPlayer
+  useEffect(() => {
+    getAllDocuments(params.courseId, params.lessonId).then((res) => {
+      if (res) {
+        if (res.status == 200) {
+          setDocuments(res.data.documents);
+        }
+      }
+    });
+  }, []);
+
+  if (!lesson || !course) {
+    return <div>Loading...</div>;
+  }
+
+  const isLocked = !lesson.isFree && !isPurchased;
+  const completeOnEnd = !!isPurchased && !lesson.finished_at;
+
+  return (
+    <div className="h-full">
+      <div className="md:pl-80 pt-[80px] h-full">
+        <div>
+          {lesson.finished_at && (
+            <Banner
+              variant="success"
+              label="You already completed this lesson."
+            />
+          )}
+          {isLocked && (
+            <Banner
+              variant="warning"
+              label="You need to purchase this course to watch this lesson."
+            />
+          )}
+          <div className="flex flex-col max-w-4xl mx-auto pb-20">
+            <div className="p-4">
+              {/*<VideoPlayer
                       chapterId={params.lessonId}
                       title={lesson.name}
                       courseId={params.courseId}
@@ -82,55 +86,52 @@ const LessonIdPage = ({
                       isLocked={isLocked}
                       completeOnEnd={completeOnEnd}
                   />*/}
-              </div>
+            </div>
             <div>
-            <div className="p-4 flex flex-col md:flex-row items-center justify-between">
-                <h2 className="text-2xl font-semibold mb-2">
-                    {lesson.name}
-                </h2>
+              <div className="p-4 flex flex-col md:flex-row items-center justify-between">
+                <h2 className="text-2xl font-semibold mb-2">{lesson.name}</h2>
                 {isPurchased ? (
-                    <CourseProgressButton
-                        lessonId={params.lessonId}
-                        courseId={params.courseId}
-                        nextLessonId={nextLesson?.id}
-                        isCompleted={!!lesson.finished_at}
-                    />
+                  <CourseProgressButton
+                    lessonId={params.lessonId}
+                    courseId={params.courseId}
+                    nextLessonId={nextLesson?.id}
+                    isCompleted={!!lesson.finished_at}
+                  />
                 ) : (
-                    <CourseEnrollButton
-                        courseId={params.courseId}
-                        price={course.price!}
-                    />
+                  <CourseEnrollButton
+                    courseId={params.courseId}
+                    price={course.price!}
+                  />
                 )}
-            </div>
-            <Separator />
-            <div>
+              </div>
+              <Separator />
+              <div>
                 <Preview value={lesson.name!} />
-            </div>
-            {!!documents.length && (
+              </div>
+              {!!documents.length && (
                 <>
-                <Separator />
-                <div className="p-4">
+                  <Separator />
+                  <div className="p-4">
                     {documents.map((document) => (
-                    <a 
+                      <a
                         href={document.file_path}
                         target="_blank"
                         key={document.document_id}
-                        className="flex items-center p-3 w-full bg-sky-200 border text-sky-700 rounded-md hover:underline" rel="noreferrer"
-                    >
+                        className="flex items-center p-3 w-full bg-sky-200 border text-sky-700 rounded-md hover:underline"
+                        rel="noreferrer"
+                      >
                         <File />
-                        <p className="line-clamp-1">
-                        {document.name}
-                        </p>
-                    </a>
+                        <p className="line-clamp-1">{document.name}</p>
+                      </a>
                     ))}
-                </div>
+                  </div>
                 </>
-            )}
-          </div>
-          <Separator />
-          <div>
-            <Preview value={lesson.name!} />
-          </div>
+              )}
+            </div>
+            <Separator />
+            <div>
+              <Preview value={lesson.name!} />
+            </div>
             {!!documents.length && (
               <>
                 <Separator />
@@ -152,9 +153,9 @@ const LessonIdPage = ({
             )}
           </div>
         </div>
-        </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default LessonIdPage;
